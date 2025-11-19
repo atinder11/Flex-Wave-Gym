075baf12044c3c4b85c911f53434db7b55cbce03
Atinder Kumar <atinderkumar1111@gmail.com>
2025-11-19 23:29:58 +0530

    added the pricing section

--- src/components/Program.tsx
+++ src/components/Program.tsx
@@ -12,7 +12,7 @@ function Program() {
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-10">
             <h2 className="text-black text-3xl font-bold text-center md:text-left dark:text-white">
-              Our Program <br /> designed for you
+              Our Program is <br /> designed for you
             </h2>
 
             <p className="text-xl font-semibold text-center md:text-left">
